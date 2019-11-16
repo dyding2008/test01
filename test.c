be26a08b09a1e111a7bbfc64a7e88eb3614262b5
DDY <dyding2008@gmail.com>
2019-11-17 00:01:16 +0100

    add test.c file

---
+++ test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//using namespace std;
+
+void printMat(int m, int n, const int mat[][n])
+{
+  //cout<<mat[0][0]<<endl;
+    int ii = 0, jj = 0;
+    for(ii=0;ii<m;ii++)
+        for(jj=0;jj<n;jj++)
+            printf("%d\n", mat[ii][jj]);
+}
+
+
+
+int main()
+{
+  int aa[3][2] = {{1,2},{3,4},{5,6}};
+  
+  printMat(3,2,aa);
+  //printMat2(aa,3,2);
+  
+  return 0;
+}
